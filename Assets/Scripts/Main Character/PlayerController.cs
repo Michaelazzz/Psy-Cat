7058e2a27ee8a9fb8f179b05b9e79ede658252c6
Michaela van Zyl <michaelavz1996@gmail.com>
2018-06-28 16:19:23 +0200

    Tweaks and scene transitions

--- Assets/Scripts/Main Character/PlayerController.cs
+++ Assets/Scripts/Main Character/PlayerController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour 
 {
 	//movement
+	[Header("Movement")]
 	[SerializeField]
 	private float speed = 10f;					//how fast the player moves
 	[SerializeField]
@@ -14,6 +16,7 @@ public class PlayerController : MonoBehaviour
 	Animator animator;							//get reference to the animator
 
 	//jump
+	[Header("Jump")]
 	[SerializeField]
 	private float jumpSpeed = 10f;
 	[SerializeField]
@@ -23,6 +26,7 @@ public class PlayerController : MonoBehaviour
 	private bool jumpRequest = false;
 
 	//ground
+	[Header("Ground")]
 	[SerializeField]
 	private Transform[] groundPoints;
 	[SerializeField]
@@ -31,38 +35,57 @@ public class PlayerController : MonoBehaviour
 	public bool grounded;
 
 	//pushable box
+	[Header("Pushable Box")]
 	public float raycastDistance = 1.2f;		//distance raycast will be projected
 	public LayerMask boxMask;
 	private bool pushpull = false;				// if the player is currently pushing or pulling
 	private GameObject box;						//the box the raycast has hit
 	private float tempSpeed;
-	RaycastHit2D hit;							//raycast to detect collision
+	RaycastHit2D hit;							//raycast to detect collision with box
 	private bool forward = true;
 
-	//sound effects
-	private AudioSource source;
-	public AudioClip jumpSound;
-	public float volLowRange = 0.5f;
-	public float volHighRange = 1f;
-
 	//enemy
+	[Header("Enemy")]
 	public float enemyDetectionArea = 0.5f;
 	public LayerMask enemyMask;
 	RaycastHit2D enemyHitLeft;
 	RaycastHit2D enemyHitRight;
-	bool dead = false;
+	public bool dead = false;
+
+	//environment detection
+	[Header("Environment Detection")]
+	public BoxCollider2D environmentCollider;
+	//public float environmentDetectionArea = 1.2f;
+	public LayerMask environmentMask;
+	//RaycastHit2D environmentHit;
+
+	//scene transition
+	[Header("Scene Transition")]
+	public Animator transitionAnim;
+	public string sceneName;
+
+	//sound effects
+	[Header("Sound Effects")]
+	public AudioClip[] jumpSounds;
+	public float volLowRange = 0.5f;
+	public float volHighRange = 1f;
+	private AudioSource source;
 
 	void Awake()
 	{
 		//get audio source from player
 		source = GetComponent<AudioSource>();
 
+		//pushable box
 		tempSpeed = speed;
 		hit = Physics2D.Raycast (transform.position, Vector2.left, raycastDistance, boxMask);
 
 		//enemy detection raycasts
 		enemyHitLeft = Physics2D.Raycast (transform.position, Vector2.left, enemyDetectionArea, enemyMask);
 		enemyHitRight = Physics2D.Raycast (transform.position, Vector2.right, enemyDetectionArea, enemyMask);
+
+		//environment detection
+		//environmentHit = Physics2D.Raycast (transform.position, Vector2.left, environmentDetectionArea, environmentMask);
 
 		playerRigidBody = GetComponent<Rigidbody2D> ();
 		animator = GetComponent<Animator> ();
@@ -72,10 +95,12 @@ public class PlayerController : MonoBehaviour
 	void Update()
 	{
 		//jump
-		if (Input.GetButtonDown ("Jump")) {
+		if (Input.GetButtonDown ("Jump") && grounded) {
 			//play jump sound
 			float vol = Random.Range(volLowRange, volHighRange);
-			source.PlayOneShot (jumpSound, vol);
+
+			//get random number between 0 and size of the array and play sound
+			source.PlayOneShot (jumpSounds[Random.Range(0, jumpSounds.Length)], vol);
 
 			jumpRequest = true;
 		}
@@ -98,6 +123,12 @@ public class PlayerController : MonoBehaviour
 			//get movement direction
 			float horizontal = Input.GetAxis ("Horizontal");
 
+			//if button is being pressed and front of character is colliding with the environment
+			if (environmentCollider.IsTouchingLayers(environmentMask) && Mathf.Abs(horizontal) > 0f) 
+			{
+				horizontal = 0;
+			}
+
 			//add velocity to the rigid body in the direction of the movement * our speed
 			playerRigidBody.velocity = new Vector2 (horizontal * speed, playerRigidBody.velocity.y);
 
@@ -113,10 +144,13 @@ public class PlayerController : MonoBehaviour
 			//push box collision detecting
 			Physics2D.queriesStartInColliders = false;
 
-			if (!facingRight) {
+			if (!facingRight) 
+			{
 				//push box raycast left
 				hit = Physics2D.Raycast (transform.position, Vector2.left, raycastDistance, boxMask);
-			} else {
+			} 
+			else 
+			{
 				//push box raycast right
 				hit = Physics2D.Raycast (transform.position, Vector2.right, raycastDistance, boxMask);
 			}
@@ -149,14 +183,16 @@ public class PlayerController : MonoBehaviour
 			}
 
 			//if colliding with object and push/pull button is being pressed
-			if (hit.collider != null && Input.GetButtonDown ("PushPull")) {
+			if (hit.collider != null && Input.GetButtonDown ("PushPull")) 
+			{
 				pushpull = true;
 				box = hit.collider.gameObject;
 				box.GetComponent<FixedJoint2D> ().enabled = true;
 				box.GetComponent<FixedJoint2D> ().connectedBody = playerRigidBody;
 
 				//Debug.Log ("Pushing/Pulling");
-			} else if (!Input.GetButton ("PushPull") && box != null) { //alternative -> Input.GetButtonUp ("PushPull")
+			} 
+			else if (!Input.GetButton ("PushPull") && box != null) { //alternative -> Input.GetButtonUp ("PushPull")
 				pushpull = false;
 				box.GetComponent<FixedJoint2D> ().enabled = false;
 			}
@@ -166,6 +202,7 @@ public class PlayerController : MonoBehaviour
 			animator.SetBool ("pushpull", pushpull);
 			animator.SetBool ("grounded", grounded);
 			animator.SetFloat ("velocityX", Mathf.Abs (horizontal));
+			animator.SetFloat ("rigidbodyVelocityX", playerRigidBody.velocity.x);
 			animator.SetFloat ("velocityY", velocityY);
 			animator.SetBool ("dead", dead);
 		}
@@ -183,25 +220,34 @@ public class PlayerController : MonoBehaviour
 	void jump()
 	{
 		//if button is pressed and player is grounded
-		if (jumpRequest && grounded) {
+		if (jumpRequest && grounded) 
+		{
 			//playerRigidBody.velocity = Vector2.up * jumpSpeed;
 			playerRigidBody.AddForce (Vector2.up * jumpSpeed, ForceMode2D.Impulse);
 
 			//if player is falling
-			if (playerRigidBody.velocity.y < 0) {
+			if (playerRigidBody.velocity.y < 0) 
+			{
 				playerRigidBody.gravityScale = fallMultiplier;
 				//alternative
 				//playerRigidBody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-			} else {
+			} 
+			else 
+			{
 				playerRigidBody.gravityScale = 1f;
 			}
 			jumpRequest = false;
-		} else if (!Input.GetButton ("Jump")) { //cancel jump if player lets go
-			if (playerRigidBody.velocity.y > 0) {
+		} 
+		else if (!Input.GetButton ("Jump")) //cancel jump if player lets go
+		{ 
+			if (playerRigidBody.velocity.y > 0) 
+			{
 				playerRigidBody.gravityScale = lowJumpMultiplier;
 				//alternative
 				//playerRigidBody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
-			} else {
+			} 
+			else 
+			{
 				playerRigidBody.gravityScale = 1f;
 			}
 		}
@@ -213,20 +259,25 @@ public class PlayerController : MonoBehaviour
 		enemyHitRight = Physics2D.Raycast (transform.position, Vector2.right, enemyDetectionArea, enemyMask);
 
 		//player dies
-		if (enemyHitLeft.collider != null || enemyHitRight.collider != null){
+		if (enemyHitLeft.collider != null || enemyHitRight.collider != null)
+		{
 			dead = true;
+			StartCoroutine (LoadScene());
 		}
 	}
 
 	private bool isGrounded()
 	{
 		//falling or on ground
-		if (playerRigidBody.velocity.y <= 0) {
-			foreach (Transform point in groundPoints) {
+		if (playerRigidBody.velocity.y <= 0) 
+		{
+			foreach (Transform point in groundPoints) 
+			{
 				Collider2D[] colliders = Physics2D.OverlapCircleAll (point.position, groundRadius, ground);
-
-				for (int i = 0; i < colliders.Length; i++) {
-					if (colliders [i].gameObject != null) {
+				for (int i = 0; i < colliders.Length; i++) 
+				{
+					if (colliders [i].gameObject != null) 
+					{
 						return true;
 					}
 				}
@@ -234,18 +285,26 @@ public class PlayerController : MonoBehaviour
 		}
 		return false;
 	}
+
+	IEnumerator LoadScene()
+	{
+		transitionAnim.SetTrigger ("dead");
+		yield return new WaitForSeconds (1.5f);
+		SceneManager.LoadScene (sceneName);
+	}
 		
 	void OnDrawGizmos()
 	{
 		Gizmos.color = Color.yellow;
 
+		//box detection
 		if(!facingRight)
 			Gizmos.DrawLine (transform.position, (Vector2)transform.position + Vector2.left * raycastDistance);
 		else if(facingRight)
 			Gizmos.DrawLine (transform.position, (Vector2)transform.position + Vector2.right * raycastDistance);
 
 		//enemy detection
-		Gizmos.DrawLine (transform.position, (Vector2)transform.position + Vector2.left * enemyDetectionArea);
-		Gizmos.DrawLine (transform.position, (Vector2)transform.position + Vector2.right * enemyDetectionArea);
+		//Gizmos.DrawLine (transform.position, (Vector2)transform.position + Vector2.left * enemyDetectionArea);
+		//Gizmos.DrawLine (transform.position, (Vector2)transform.position + Vector2.right * enemyDetectionArea);
 	}
 }
---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour {
+
+	public Animator transitionAnim;
+	public string sceneName;
+
+	void Update()
+	{
+		//condition for change
+		//StartCoroutine (LoadScene());
+	}
+
+	IEnumerator LoadScene()
+	{
+		transitionAnim.SetTrigger ("dead");
+		yield return new WaitForSeconds (1.5f);
+		SceneManager.LoadScene (sceneName);
+	}
+}
