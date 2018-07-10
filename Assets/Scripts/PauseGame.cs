2ddbb1b04407165ee51c26355ae1d0c9dc335037
Michaela van Zyl <michaelavz1996@gmail.com>
2018-07-10 17:39:34 +0200

    Tutorial Level Adjustments

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+
+	public Transform canvas;
+
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (canvas.gameObject.activeInHierarchy == false) 
+			{
+				canvas.gameObject.SetActive(true);
+				Time.timeScale = 0;
+				AudioListener.volume = 0;
+			} 
+			else 
+			{
+				canvas.gameObject.SetActive(false);
+				Time.timeScale = 1;
+				AudioListener.volume = 1;
+			}
+		}
+	}
+}
