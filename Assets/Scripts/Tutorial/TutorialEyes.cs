4a501bc2e3aefbd9bd33ff80f49bb0d3db1caa65
Michaela van Zyl <michaelavz1996@gmail.com>
2018-07-12 11:43:17 +0200

    Tutorial
    Background eyes animation and triggers

---
+++ Assets/Scripts/Tutorial/TutorialEyes.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialEyes : MonoBehaviour {
+
+	//variables
+	public Animator eyeAnim;					//animator for the eye
+	public bool tutorialPoint = false;			//if player has hit a point where they nedd instructions
+	bool blink = true;							//if blink time has been assigned
+	public BoxCollider2D point;					//tutorial point linked to the eye
+	public LayerMask playerMask;
+
+	void Update()
+	{
+		if (point.IsTouchingLayers (playerMask)) {
+			tutorialPoint = true;
+		} else {
+			tutorialPoint = false;
+		}
+
+		if (tutorialPoint) {
+			eyeAnim.SetBool ("tutorial", true);
+		} else {
+			eyeAnim.SetBool ("tutorial", false);
+			StartCoroutine (Blink());
+		}
+	}
+
+	IEnumerator Blink()
+	{
+		float time = randomTime ();
+		if(blink)
+		{
+			blink = false;
+			Debug.Log (time);
+			yield return new WaitForSeconds (time);
+			eyeAnim.SetTrigger ("close");
+			blink = true;
+		}
+	}
+
+	public float randomTime()
+	{
+		return Random.Range (4, 10);
+	}
+}
---
+++ Assets/Scripts/Tutorial/TutorialPoints.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPoints : MonoBehaviour {
+
+	//variables
+	public Animator eyeAnim;			//animator for the eye
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag ("Player")) {
+			eyeAnim.SetBool ("tutorial", true);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		eyeAnim.SetBool ("tutorial", false);
+	}
+}
