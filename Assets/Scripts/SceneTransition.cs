81c1de00b236a101fc1275c034c8b88cc9622831
Michaela van Zyl <michaelavz1996@gmail.com>
2018-07-21 11:15:38 +0200

    Tutorial Level
    Started cutscene

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -6,11 +6,16 @@ using UnityEngine.SceneManagement;
 public class SceneTransition : MonoBehaviour {
 
 	//scene transition
+	[Header("Scene Transistion")]
 	public Animator transitionAnim;
 	public string sceneName;
 
 	//music transition
+	[Header("Music Transition")]
 	public Animator musicAnim;
+
+	//game master
+	public GameObject gameMaster;
 
 	void Update()
 	{
@@ -23,6 +28,13 @@ public class SceneTransition : MonoBehaviour {
 		musicAnim.SetTrigger ("mute");
 		transitionAnim.SetTrigger ("dead");
 		yield return new WaitForSeconds (1.5f);
+		//destroy game master
+		Destroy(gameMaster);
 		SceneManager.LoadScene (sceneName);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		StartCoroutine (LoadScene());
 	}
 }
