5969e75e4d093d44d8e1ab0bb96df87557abe060
Michaela van Zyl <michaelavz1996@gmail.com>
2018-08-05 17:05:17 +0200

    Main Menu
    Video and layout

---
+++ Assets/Scripts/UI/MainMenuPlay.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class MainMenuPlay : MonoBehaviour {
+
+	//scene transition
+	[Header("Scene Transistion")]
+	public Animator transitionAnim;
+	public string sceneName;
+	public float time = 1.5f;
+
+	//music transition
+	[Header("Music Transition")]
+	public Animator musicAnim;
+
+	[Header("Video")]
+	public VideoPlayer video;
+
+	public void Play()
+	{
+		StartCoroutine (LoadScene ());
+	}
+
+	IEnumerator LoadScene()
+	{
+		video.Play ();
+		if(musicAnim != null)
+			musicAnim.SetTrigger ("mute");
+		transitionAnim.SetTrigger ("dead");
+		yield return new WaitForSeconds (time);
+		SceneManager.LoadScene (sceneName);
+	}
+}
