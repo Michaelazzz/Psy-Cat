08eaa3b9978e461c5d1f3c300c66ef0cf5ac3afe
Michaela van Zyl <michaelavz1996@gmail.com>
2018-08-05 15:57:35 +0200

    Tutorial Level
    Tweeks implemented

--- Assets/Scripts/Main Character/CameraController.cs
+++ Assets/Scripts/Main Character/CameraController.cs
@@ -8,16 +8,25 @@ public class CameraController : MonoBehaviour {
 	public float smoothSpeed = 10f;
 	public Vector3 offset;
 
+	public float cameraMultiplier = 1;
+	Vector3 inputDirection;
+
 	void Awake()
 	{
 		target = GameObject.Find ("Player").transform;
 		transform.position = target.position;
 	}
 
+	void Update()
+	{
+		inputDirection.x = Input.GetAxis ("RightStickXAxis") * cameraMultiplier;
+		inputDirection.y = Input.GetAxis ("RightStickYAxis") * cameraMultiplier;
+	}
+
 	//when this function is called the character is already done with its movement
 	void LateUpdate()
 	{
-		Vector3 desiredPosition = target.position + offset;
+		Vector3 desiredPosition = target.position + offset + inputDirection;
 		Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
 		transform.position = smoothedPosition;
 	}
