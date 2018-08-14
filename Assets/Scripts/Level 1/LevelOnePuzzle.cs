487d2308892630066b1f3c215398abf8267783c5
Michaela van Zyl <michaelavz1996@gmail.com>
2018-08-14 12:36:44 +0200

    Level 1
    Puzzle implementation

---
+++ Assets/Scripts/Level 1/LevelOnePuzzle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOnePuzzle : MonoBehaviour {
+
+	//puzzle zone objects
+	public GameObject puzzleZoneRealm1;
+	public GameObject puzzleZoneRealm2;
+	public GameObject puzzleZoneRealm3;
+
+	//puzzle trigger
+	bool realm1PuzzleTrigger = false;
+	bool realm2PuzzleTrigger = false;
+	bool realm3PuzzleTrigger = false;
+
+	//puzzle mask
+	public LayerMask puzzleMask;
+
+	//eye door
+	public Animator eyeDoorAnim;
+
+	void Update()
+	{
+		if (puzzleZoneRealm1.GetComponent<BoxCollider2D> ().IsTouchingLayers (puzzleMask)) {
+			realm1PuzzleTrigger = true;
+		}
+
+		if (puzzleZoneRealm2.GetComponent<BoxCollider2D> ().IsTouchingLayers (puzzleMask)) {
+			realm2PuzzleTrigger = true;
+		}
+
+		if (puzzleZoneRealm3.GetComponent<BoxCollider2D> ().IsTouchingLayers (puzzleMask)) {
+			realm3PuzzleTrigger = true;
+		}
+
+		if (realm1PuzzleTrigger && realm2PuzzleTrigger && realm3PuzzleTrigger)
+			eyeDoorAnim.SetTrigger ("open");
+	}
+}
