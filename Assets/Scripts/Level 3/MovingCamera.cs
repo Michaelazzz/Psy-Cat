6b0f9a98361eb361e50984853f29b2f15892b088
Michaela van Zyl <michaelavz1996@gmail.com>
2018-09-25 16:09:08 +0200

    Level 3
    Platform tweeks and moving camera implementation and music.

---
+++ Assets/Scripts/Level 3/MovingCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCamera : MonoBehaviour {
+
+	public GameObject movingCamera;
+	public float time;
+	public float velocity;
+	public Transform currentPosition;
+	public Transform[] points;
+	public int pointSelection;
+
+	void Start()
+	{
+		currentPosition = points [pointSelection];
+	}
+
+	void Update()
+	{
+		StartCoroutine (StartMovingCamera ());
+	}
+
+	IEnumerator StartMovingCamera()
+	{
+		yield return new WaitForSeconds (time);
+
+		movingCamera.transform.position = Vector3.MoveTowards (movingCamera.transform.position, currentPosition.position, velocity * Time.deltaTime);
+
+		//if (movingCamera.transform.position == currentPosition.position) {
+		//	pointSelection++;
+
+		//	if (pointSelection == points.Length)
+		//		pointSelection = 0;
+
+		//	currentPosition = points [pointSelection];
+		//}
+	}
+}
