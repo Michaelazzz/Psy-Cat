5e9e77a30cce6c11c9bb51c5602cdbab5378ade2
Michaela van Zyl <michaelavz1996@gmail.com>
2018-10-13 10:54:57 +0200

    Logo scene and level 2
    Tweeks and bug fixes

---
+++ Assets/Scripts/UI/LogoSceneTransition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LogoSceneTransition : MonoBehaviour {
+
+	//scene transition
+	[Header("Scene Transistion")]
+	public Animator transitionAnim;
+	public string sceneName;
+	public float time = 8f;
+
+	void Start()
+	{
+		StartCoroutine (LoadScene());
+	}
+
+	IEnumerator LoadScene()
+	{
+		transitionAnim.SetTrigger ("dead");
+		yield return new WaitForSeconds (time);
+		SceneManager.LoadScene (sceneName);
+	}
+}
